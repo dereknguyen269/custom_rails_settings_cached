bfdfb35fb78905f0baf52579bf97d61dffe591eb
Derek <quanmn.libra@gmail.com>
2021-03-21 11:06:58 +0700

    v0.1.0

---
+++ lib/custom_rails_settings_cached.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require_relative "custom_rails_settings_cached/version"
+
+module CustomRailsSettingsCached
+  extend RailsSettings::Extend
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :settings, as: :thing
+
+    def get_settings(name)
+      if has_associations_with_rails_settings
+        st = settings.find_by var: setting_key(name)
+        st&.value
+      else
+        Setting[setting_key(name)]
+      end
+    end
+
+    def init_setting(name, value)
+      if has_associations_with_rails_settings
+        st = settings.find_or_initialize_by(var: setting_key(name))
+        st.value = value
+        st.save
+      else
+        Setting[setting_key(name)] = value
+      end
+    end
+
+    def setting_key(name)
+      return name.to_s.underscore if has_associations_with_rails_settings
+      "#{self.class.name}.#{self.id}.#{name}".underscore
+    end
+
+    def settings_as_object
+      data = { partner_id: self.id }
+      settings.each do |st|
+        data[pretty_key(st.var)] = st.value
+      end
+      OpenStruct.new data
+    end
+
+    def pretty_key(key_name)
+      key_name.split('.').last
+    end
+
+    def has_associations_with_rails_settings
+      return @has_rails_settings unless @has_rails_settings.nil?
+      associations = self.class.reflect_on_all_associations(:has_many)
+      @has_rails_settings = associations.any? { |a| a.name == :settings }
+    end
+
+    if defined?(self::CUSTOM_RAILS_SETTINGS_KEYS)
+      if self::CUSTOM_RAILS_SETTINGS_KEYS.is_a?(Array)
+        # Exam:
+        # CUSTOM_RAILS_SETTINGS_KEYS = [:google_analytics, :facebook_pixel_ads]
+        self::CUSTOM_RAILS_SETTINGS_KEYS.each do |setting_key|
+          self.define_method "#{setting_key}" do
+            self.get_settings setting_key
+          end
+          self.define_method "#{setting_key}=" do |setting_value|
+            self.init_setting(setting_key, setting_value)
+          end
+        end
+      elsif self::CUSTOM_RAILS_SETTINGS_KEYS.is_a?(Hash)
+        # Exam:
+        # CUSTOM_RAILS_SETTINGS_KEYS = {
+        #   google_analytics: [:enabled, :tracking_code],
+        #   facebook_pixel_ads: [:enabled, :tracking_code]
+        # }
+        self::CUSTOM_RAILS_SETTINGS_KEYS.each do |k, v|
+          if v.is_a?(Array)
+            v.each do |setting_key|
+              # Use: Partner.first.google_analytics_enabled
+              store_setting_key = "#{k}_#{setting_key}"
+              self.define_method store_setting_key do
+                self.get_settings store_setting_key
+              end
+              # Use: Partner.first.google_analytics_enabled = true
+              self.define_method "#{store_setting_key}=" do |setting_value|
+                self.init_setting(store_setting_key, setting_value)
+              end
+            end
+          else
+            # Use: Partner.first.google_analytics
+            self.define_method "#{v}" do
+              self.get_settings v
+            end
+            self.define_method "#{v}=" do |setting_value|
+              self.init_setting(v, setting_value)
+            end
+          end
+        end
+      end
+    end
+  end
+end
